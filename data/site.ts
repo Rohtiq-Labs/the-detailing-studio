7d22ded4a6b48c139aa4f3ba828141c876bb2f50
Muhammad Taha <muhammad9675581@gmail.com>
2026-07-12 07:30:13 +0500

    email upd

--- data/site.ts
+++ data/site.ts
@@ -4,7 +4,7 @@ export const SITE = {
   domain: "detailingstudio.ca",
   phone: "647-836-5477",
   phoneTel: "6478365477",
-  email: "hello@detailingstudio.ca",
+  email: "Detailingstudio.ca@gmail.com",
   instagram: "https://instagram.com/detailingstudio.ca",
   tagline: "Where Precision Meets Passion",
   est: "EST. GTA",
